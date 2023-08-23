12e991e915a39298f0fe9b8a7f0062d32c7bc46c
Eduard Takhunts <eduardtakhunts@eduards-air.myfiosgateway.com>
2023-08-23 06:16:02 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
 require('dotenv').config();
 const express = require('express');
+const { Client } = require('pg');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 app.use(cors({
-  origin: 'http://127.0.0.1:3000', // Ensure this matches the client's URL
+  origin: 'http://localhost:3000', // Client's URL (matching the client's port)
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
-  optionsSuccessStatus: 204,
 }));
 
-app.use(express.json());
-
-// Simple GET Endpoint for Testing
-app.get('/test', (req, res) => {
-  res.status(200).json({ message: 'Test GET successful' });
-  res.send('Server is working');
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
-// Other Endpoints...
-// ...
+client.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Connection error', err.stack));
 
-const PORT = 4001;
-const HOST = '127.0.0.1'; // Explicitly set the host
+// Registration Route
+app.post('/register', async (req, res) => {
+  console.log(req.body);
+  try {
+    const { email, password } = req.body;
+    const result = await client.query('SELECT * FROM users WHERE email = $1', [email]);
+    const existingUser = result.rows[0];
+    if (existingUser) {
+      return res.status(400).json({ error: 'Email already registered' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await client.query('INSERT INTO users (email, password) VALUES ($1, $2)', [email, hashedPassword]);
+    res.status(200).json({ success: 'Registration successful!' });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
-app.listen(PORT, HOST, function() {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, function() {
+  console.log('Server is running on port 3000');
 });
